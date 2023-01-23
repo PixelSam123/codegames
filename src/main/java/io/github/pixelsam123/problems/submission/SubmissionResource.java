1c7df32cf4bbf80f21074d3ac0dc1517ac6bc1c5
PixelSam123 <77851256+PixelSam123@users.noreply.github.com>
2023-01-23 15:34:02 +0700

    add submissions into database

--- src/main/java/io/github/pixelsam123/problems/submission/SubmissionResource.java
+++ src/main/java/io/github/pixelsam123/problems/submission/SubmissionResource.java
@@ -1,5 +1,6 @@
 package io.github.pixelsam123.problems.submission;
 
+import io.agroal.api.AgroalDataSource;
 import io.github.pixelsam123.problems.ProblemService;
 import io.github.pixelsam123.problems.submission.runner.ISubmissionRunner;
 import io.github.pixelsam123.problems.submission.runner.JavetSubmissionRunner;
@@ -15,6 +16,9 @@ import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.SecurityContext;
 import java.net.URI;
+import java.security.Principal;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.Map;
 
 @Path("/problems/v1/submission")
@@ -22,40 +26,66 @@ public class SubmissionResource {
 
     private final ProblemService problemService;
     private final ISubmissionRunner submissionRunner;
+    private final AgroalDataSource dataSource;
 
     public SubmissionResource(
         ProblemService problemService,
-        JavetSubmissionRunner submissionRunner
+        JavetSubmissionRunner submissionRunner,
+        AgroalDataSource dataSource
     ) {
         this.problemService = problemService;
         this.submissionRunner = submissionRunner;
+        this.dataSource = dataSource;
     }
 
     @POST
     @Path("/{idx}")
     @Consumes(MediaType.TEXT_PLAIN)
     @Produces(MediaType.APPLICATION_JSON)
-    public Uni<Response> postOne(@Context SecurityContext loggedInUser, String code, int idx) {
+    public Uni<Response> postOne(@Context SecurityContext securityCtx, String code, int idx) {
         return problemService
             .getOneByIdx(idx)
             .onItemOrFailure()
             .transformToUni((problem, error) -> {
                 if (error instanceof IndexOutOfBoundsException) {
-                    return Uni.createFrom().item(() -> Response
-                        .status(Response.Status.BAD_REQUEST)
-                        .entity(Map.ofEntries(
-                            Map.entry("content", "Invalid index")
-                        ))
-                        .build());
+                    return Uni
+                        .createFrom()
+                        .item(() -> Response
+                            .status(Response.Status.BAD_REQUEST)
+                            .entity(Map.ofEntries(Map.entry("content", "Invalid index")))
+                            .build());
                 }
 
                 return submissionRunner
                     .run(code + '\n' + problem.tests())
                     .runSubscriptionOn(Infrastructure.getDefaultWorkerPool())
-                    .map(result -> Response
-                        .created(URI.create("/problems/v1/submission/" + idx))
-                        .entity(result)
-                        .build());
+                    .map(result -> {
+                        Principal loggedInUser = securityCtx.getUserPrincipal();
+
+                        if (loggedInUser == null) {
+                            return Response.ok(result).build();
+                        }
+
+                        try (PreparedStatement submissionInsertion = dataSource
+                            .getConnection()
+                            .prepareStatement("INSERT INTO submission VALUES (?,?,?)")) {
+                            submissionInsertion.setString(1, loggedInUser.getName());
+                            submissionInsertion.setString(2, code);
+                            submissionInsertion.setString(3, result.status().value);
+
+                            submissionInsertion.executeUpdate();
+                        } catch (SQLException err) {
+                            return Response.serverError().entity(Map.ofEntries(
+                                Map.entry("content", err.toString())
+                            )).build();
+                        }
+
+                        return Response
+                            .created(URI.create("/problems/v1/submission/" + idx))
+                            .entity(result)
+                            .build();
+                    })
+                    .runSubscriptionOn(Infrastructure.getDefaultWorkerPool());
             });
     }
 
