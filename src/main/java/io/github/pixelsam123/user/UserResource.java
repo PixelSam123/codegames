d168baf17ea16577bc59ae4dc625ee5288b4a8dd
PixelSam123 <77851256+PixelSam123@users.noreply.github.com>
2023-01-23 15:33:20 +0700

    refactor user sign up resource

--- src/main/java/io/github/pixelsam123/user/UserResource.java
+++ src/main/java/io/github/pixelsam123/user/UserResource.java
@@ -12,7 +12,6 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.net.URI;
-import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.util.Map;
@@ -30,27 +29,23 @@ public class UserResource {
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
     public Uni<Response> postOne(UserCreationView user) {
-        return Uni
-            .createFrom()
-            .item(() -> {
-                try (Connection c = dataSource.getConnection()) {
-                    PreparedStatement userInsertion = c.prepareStatement(
-                        "INSERT INTO user VALUES (?,?,?)"
-                    );
-                    userInsertion.setString(1, user.name());
-                    userInsertion.setString(2, BcryptUtil.bcryptHash(user.password()));
-                    userInsertion.setString(3, "user");
+        return Uni.createFrom().item(() -> {
+            try (PreparedStatement userInsertion = dataSource.getConnection().prepareStatement(
+                "INSERT INTO user VALUES (?,?,?)"
+            )) {
+                userInsertion.setString(1, user.name());
+                userInsertion.setString(2, BcryptUtil.bcryptHash(user.password()));
+                userInsertion.setString(3, "user");
 
-                    userInsertion.executeUpdate();
-                } catch (SQLException e) {
-                    return Response.serverError().entity(Map.ofEntries(
-                        Map.entry("content", e.toString())
-                    )).build();
-                }
+                userInsertion.executeUpdate();
+            } catch (SQLException err) {
+                return Response.serverError().entity(Map.ofEntries(
+                    Map.entry("content", err.toString())
+                )).build();
+            }
 
-                return Response.created(URI.create("/user/" + user.name())).build();
-            })
-            .runSubscriptionOn(Infrastructure.getDefaultWorkerPool());
+            return Response.created(URI.create("/user/" + user.name())).build();
+        }).runSubscriptionOn(Infrastructure.getDefaultWorkerPool());
     }
 
 }
